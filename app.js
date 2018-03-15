0b0a0a5f3e0f6640b9fe9f2b5e5421cfb9cdacb6
robin torn <robin.torn.98@gmail.com>
2018-03-15 16:50:49 +0100

    Update

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let option = document.getElementById("search-type");
 let searchq = document.getElementById("search-text");
 
 document.getElementById("search-start").addEventListener("click", function(){
-
     fetch('https://api.spotify.com/v1/search?q='+searchq.value+'&type='+option.value+'&market=US',{
         
         method: 'GET',
@@ -12,6 +11,33 @@ document.getElementById("search-start").addEventListener("click", function(){
         })
     })
     .then(response => response.json())
-    .then(spotify => console.log(spotify))
+    .then(data => DOM(data))
     .catch(err => console.log(err));
 })
+
+
+function DOM(data){
+    for(let i in data.tracks.items){
+        let create = document.getElementById("search-results");
+        let div1 = document.createElement("div");
+        let div2 = document.createElement("div");
+        let p = document.createElement("p");
+        let link = document.createElement("a");
+        let img = document.createElement("img");
+
+        create.appendChild(div1);
+        div1.appendChild(div2);
+        div1.appendChild(img);
+        div2.appendChild(p);
+        div2.appendChild(link);
+
+        div1.setAttribute("class", "search-result");
+        div2.setAttribute("class", "search-result-info");
+
+        p.innerHTML = data.tracks.items[i].name;
+        link.setAttribute("href", data.tracks.items[i].uri);
+        link.innerHTML = "Länk till spotify";
+        img.setAttribute("src", data.tracks.items[i].album.images[0].url);
+    }
+    console.log(data);
+}
--- constants.js
+++ constants.js
@@ -5,4 +5,4 @@ const API_URL       = 'https://api.spotify.com/v1/search?q=';
 const IMAGE_NA_URL  = 'http://www.hellcat.org/attachments/image-not-available-jpg.289129/';
 
 // your OAuth token. NOTE: Expires after 1h!.
-const AUTH_TOKEN    = 'Bearer BQCKR5CQKyhKwi3MckGjwjPZP9jIxQPqYXQwC2xtgZmlF6gs72QzyBjUam7goLXhCnCIX8k6KrcLfNJPvF6GoFTxljqw_afK2PzdvMdcMDk7bKylh3K9Y9E1QirTTo5n-C86W5ZNm5jVRX4Qje0';
+const AUTH_TOKEN    = 'Bearer BQDaY7qv7XtNPYdXfs62bvKVtqbxfNBLR5rT0YPF-fdXLkM06VhszYRLym_gu5IgRhy3E5IFFVhQbMII-NaG4-2Ix_RDG2ohHi0L7qfzpUbLm78SgGnNvmonIqPZ8sVhMMej6GXAfrd4plPxaRg';
